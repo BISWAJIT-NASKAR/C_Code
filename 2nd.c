2606291d9c8af92fdc0b7244a6f16a0cdb060f75
Biswajit Naskar <129007311+BISWAJIT-NASKAR@users.noreply.github.com>
2025-11-29 23:59:48 +0530

    Add files via upload

---
+++ 2nd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	float i=15.0, j=6.0;
+	printf("%f \n",i);
+	printf("%f \n",j);
+	printf("%f \n",i+j);
+	printf("%f \n",i-j);
+	printf("%f \n",j-i);
+	printf("%f \n",i*j);
+	printf("%f \n",i/j);
+	printf("%f \n",j/i);
+	return 0;
+	
+}
---
+++ 3rd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	int i=24, j=6;
+	printf("%d \n",i);
+	printf("%d \n",j);
+	printf("%d \n",i+j);
+	printf("%d \n",i-j);
+	printf("%d \n",i*j);
+	printf("%d \n",i/j);
+	printf("%d \n",i%j);
+	return 0;
+}
